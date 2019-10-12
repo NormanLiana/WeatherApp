689aa42d6248c2660039a8396f06fc4513e26228
NormanLiana <liananorman@pursuit.org>
2019-10-12 13:20:26 -0400

    adds controller folder and Weather VC cocoa file

---
+++ WeatherApp-Project/Controllers/WeatherVC.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherVC.swift
+//  WeatherApp-Project
+//
+//  Created by Liana Norman on 10/12/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
