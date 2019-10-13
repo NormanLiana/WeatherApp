ea95fe9dec5f3581235b875248e4c812b26a404e
NormanLiana <liananorman@pursuit.org>
2019-10-13 00:17:43 -0400

    sets up tab bar and nav controller with fave VC and weather VC in scene delegate, b egins to create UI objects with contraints

--- WeatherApp-Project/Controllers/WeatherVC.swift
+++ WeatherApp-Project/Controllers/WeatherVC.swift
@@ -10,10 +10,60 @@ import UIKit
 
 class WeatherVC: UIViewController {
 
+    // MARK: - UI Objects
+    lazy var cityNameLabel: UILabel = {
+        let theLabel = UILabel()
+        theLabel.backgroundColor = .blue
+        theLabel.textColor = .white
+        return theLabel
+    }()
     
+    lazy var weatherCV: UICollectionView = {
+        let cv = UICollectionView()
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        return cv
+    }()
+    
+    lazy var zipcodeTextField: UITextField = {
+        let tf = UITextField()
+        return tf
+    }()
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
+//        configureWeatherCV()
     }
     
+    // MARK: - ObjC
+    
+    
+    // MARK: - Private Methods
+    private func addSubViews() {
+        view.addSubview(cityNameLabel)
+        view.addSubview(weatherCV)
+        view.addSubview(zipcodeTextField)
+    }
+    
+    // MARK: - Contraint Methods
+    private func configureCityNameLabel() {
+        cityNameLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+//    private func configureWeatherCV() {
+//        weatherCV.translatesAutoresizingMaskIntoConstraints = false
+//
+//        [weatherCV.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor), weatherCV.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor), weatherCV.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor), weatherCV.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.5)].forEach({$0.isActive = true})
+//    }
+    
+    private func configureZipcodeTextField() {
+        zipcodeTextField.translatesAutoresizingMaskIntoConstraints = false
+    }
 
 }
+
+// MARK: - Extensions
