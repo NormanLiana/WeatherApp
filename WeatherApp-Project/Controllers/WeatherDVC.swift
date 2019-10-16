457e2327e8232a93bcb7efcb19f6404a11adf1bd
NormanLiana <liananorman@pursuit.org>
2019-10-16 17:22:05 -0400

    adds contraints for all subviews in weatherDVC

--- WeatherApp-Project/Controllers/WeatherDVC.swift
+++ WeatherApp-Project/Controllers/WeatherDVC.swift
@@ -89,11 +89,48 @@ class WeatherDVC: UIViewController {
         return iv
     }()
     
+    
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCityNameLabel()
+        configureDateLabel()
+        configureImageView()
+        configureStackView()
     }
     
-
+    // MARK: - Contraint Methods
+    private func configureCityNameLabel() {
+        cityNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(cityNameLabel)
+        
+        [cityNameLabel.topAnchor.constraint(equalTo: view.topAnchor), cityNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor), cityNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor), cityNameLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1)].forEach({$0.isActive = true})
+    }
+    
+    private func configureDateLabel() {
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(dateLabel)
+        
+        [dateLabel.topAnchor.constraint(equalTo: cityNameLabel.bottomAnchor), dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor), dateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor), dateLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1)].forEach({$0.isActive = true})
+    }
+    
+    private func configureImageView() {
+        cityImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(cityImageView)
+        
+        [cityImageView.topAnchor.constraint(equalTo: dateLabel.bottomAnchor), cityImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor), cityImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor), cityImageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)].forEach({$0.isActive = true})
+    }
+    
+    private func configureStackView() {
+        let stackView = UIStackView(arrangedSubviews: [weatherDescriptionLabel, highTempLabel, lowTempLabel, sunriseLabel, sunsetLabel, windspeedLabel, precipLabel])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        stackView.distribution = .fillEqually
+        view.addSubview(stackView)
+        
+        [stackView.topAnchor.constraint(equalTo: cityImageView.bottomAnchor), stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor), stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor), stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)].forEach({$0.isActive = true})
+    }
 
 }
