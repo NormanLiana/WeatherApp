1bbb28007da551ebf1c95f0e579be0d27af8c961
NormanLiana <liananorman@pursuit.org>
2019-10-20 15:16:47 -0400

    builds user default wrapper class

--- WeatherApp-Project/Persistence/UserDefaultsWrapper.swift
+++ WeatherApp-Project/Persistence/UserDefaultsWrapper.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class UserDefaultsWrapper {
+    
+    // MARK: - Singleton
+    static let standard = UserDefaultsWrapper()
+    
+    // MARK: - Private Keys
+    private let zipcodeKey = "zipcode"
+    
+    // MARK: - Getters
+    func getZipcode() -> String? {
+        return UserDefaults.standard.value(forKey: zipcodeKey) as? String
+    }
+    
+    // MARK: - Setters
+    func setZipcode(zipcode: String) {
+        UserDefaults.standard.set(zipcode, forKey: zipcodeKey)
+    }
+    
+}
