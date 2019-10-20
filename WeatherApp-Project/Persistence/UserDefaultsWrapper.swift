18eb9d20a9736099ca55f42047808ffda0181503
NormanLiana <liananorman@pursuit.org>
2019-10-20 09:47:39 -0400

    adds Persistence folder with user default and file manager files

---
+++ WeatherApp-Project/Persistence/UserDefaultsWrapper.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaultsWrapper.swift
+//  WeatherApp-Project
+//
+//  Created by Liana Norman on 10/20/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import Foundation
